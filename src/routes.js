cff2403911e2f4026c0c76b35deae0c454237eeb
root <alexsouzasilvax@gmail.com>
2020-01-03 20:47:17 -0300

    Ajustando porta Heroku

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const LoginController = require('./controllers/LoginController');
+const LoginController = require("./controllers/LoginController");
 
 const routes = express.Router();
 
-routes.post('/usuarios/apagar', LoginController.destroy);
+routes.post("/usuarios/apagar", LoginController.destroy);
 //routes.post('/usuarios/atualizar', LoginController.update);
-routes.post('/usuarios/criar', LoginController.store);
-routes.post('/usuarios/buscar', LoginController.show);
-routes.get('/usuarios', LoginController.index);//listar
+routes.post("/usuarios/criar", LoginController.store);
+routes.post("/usuarios/buscar", LoginController.show);
+routes.get("/usuarios", LoginController.index); //listar
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const routes = require('./routes');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-1bzmc.mongodb.net/semana09?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack-1bzmc.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+    useUnifiedTopology: true
+  }
+);
 
 //app.use(cors({ origin: 'http://localhost:3333' })); //configura quais ips poderam acessar a api
 app.use(cors());
@@ -18,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
